a040f66411d9329a64931ff43435531e1ed54bb5
ranjeet <rk369620@gmail.com>
2026-01-18 08:17:23 +0100

    refactor: added resolution settings to video input module

--- src/video_input/video_input.py
+++ src/video_input/video_input.py
@@ -1,9 +1,12 @@
-# src/video_input/video_input.py
+
 import cv2
 
 class VideoInput:
-    def __init__(self, source):
+    def __init__(self, source, width=None, height=None):
         self.cap = cv2.VideoCapture(source)
+        if width and height:
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     def get_frame(self):
         ret, frame = self.cap.read()
