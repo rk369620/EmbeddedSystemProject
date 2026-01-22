9de30da6e9abdd6dd9dce5b3f15a4b91ceb69ba5
ranjeet <rk369620@gmail.com>
2026-01-22 18:51:30 +0100

    made some small changes in vechicle_accident_detector

--- src/detection/vehicle_accident_detector.py
+++ src/detection/vehicle_accident_detector.py
@@ -5,9 +5,9 @@ from datetime import datetime, timedelta
 class VehicleAccidentDetector:
     def __init__(
         self,
-        threshold=25,
-        min_consecutive_frames=3,
-        cooldown_seconds=5
+        threshold,
+        min_consecutive_frames,
+        cooldown_seconds
     ):
         self.threshold = threshold
         self.min_consecutive_frames = min_consecutive_frames
@@ -22,15 +22,26 @@ class VehicleAccidentDetector:
 
         if self.prev_gray is None:
             self.prev_gray = gray
+            self.motion_counter = 0
             return False, None
 
         motion_score = self._compute_motion(self.prev_gray, gray)
+
+    #    print(
+     #       f"[DEBUG] motion_score={motion_score:.4f}, "
+      #      f"counter={self.motion_counter}, "
+       #     f"threshold={self.threshold}"
+       # )
+
         self.prev_gray = gray
 
         if motion_score > self.threshold:
             self.motion_counter += 1
         else:
             self.motion_counter = 0
+
+     #   if motion_score > self.threshold:
+      #      print("🚨 motion_score high, checking alert conditions...")
 
         if self._should_trigger_alert():
             self.last_alert_time = datetime.now()
@@ -43,10 +54,15 @@ class VehicleAccidentDetector:
         return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     def _compute_motion(self, prev_gray, gray):
+        h, w = gray.shape
+        prev_gray = prev_gray
+        gray = gray
+
         flow = cv2.calcOpticalFlowFarneback(
             prev_gray, gray, None,
             0.5, 3, 15, 3, 5, 1.2, 0
         )
+
         mag, _ = cv2.cartToPolar(flow[..., 0], flow[..., 1])
         return float(np.mean(mag))
 
